51428a6ce1fcded1e90165088f5a01c76c27a464
chaymae <shimamoujahid.com>
2025-03-25 12:44:56 -0700

    Add project files

---
+++ components/nav.php
@@ -0,0 +1,11 @@
+<nav class="navbar">
+    <div class="logo"><h1><i>PresenScan</i></h1></div>
+       
+        <div class="content">
+            <h2><i><b>Kan Ya Makan Redda
+            </b></i></h2>
+
+        </div>
+
+    
+   </nav>
